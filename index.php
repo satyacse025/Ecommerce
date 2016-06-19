ea48cc04af1a178098f2fb6bf80ecc48fe1088bb
satyacse025 <satyacse025@gmail.com>
2016-06-19 20:31:53 +0600

    First
    First

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+
+include("admin/page/connection.php"); 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>E-Commerce.com</title>
+
+    <!-- Bootstrap -->
+     <link rel="icon" href="image/e-commerce.png" type="image/png" sizes="32x32"> 
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/hover_style.css" rel="stylesheet">
+     <link href="css/headers.css" rel="stylesheet">
+  
+  </head>
+  <body>
+    
+
+    <?php 
+      include 'page/header.php';
+     ?>
+      <?php 
+      include 'page/slider.php';
+     ?>
+     <?php 
+      include 'page/body.php';
+     ?>
+     <?php 
+      include 'page/footer.php';
+     ?>
+
+    <script src="js/bootstrap.min.js"></script>
+     <script src="js/npm.js"></script>
+
+  </body>
+</html>
